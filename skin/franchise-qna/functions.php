dec1e8b3efa5097ef62e0f94af847d38a2bd2d48
Sunny Shoplic <sunny@shoplic.kr>
2024-05-23 17:02:42 +0900

    창업문의 시 이메일 작업

--- skin/franchise-qna/functions.php
+++ skin/franchise-qna/functions.php
@@ -50,3 +50,68 @@ function dalia_franchise_qna_kboard_get_email(&$content) {
 function dalia_franchise_qna_kboard_get_tel(&$content) {
 	return $content->option->{'tel'} ?: '';
 }
+
+function dalia_franchise_qna_send_email_when_user_ask_question($content_uid, $board_id, $content, $board) {
+	if (dalia_is_admin()) {
+		return;
+	}
+
+	$title = $content->title;
+    $url = new KBUrl();
+
+	$emails = ["rpf5573@gmail.com", "rpf5573@kakao.com"];
+
+    // Create the mail instance and set common properties
+    $mail = kboard_mail();
+    $mail->to = $emails;
+	// $mail->to = "rpf5573@gmail.com";
+    $mail->title = '[' . __('KBoard new document', 'kboard') . '] ' . $board->board_name . ' - ' . $title;
+    $mail->content = $content->getDocumentOptionsHTML() . $content->content;
+    $mail->url = $url->getDocumentRedirect($content->uid);
+    $mail->url_name = '게시물 보기';
+    $mail->send();
+}
+add_action( 'kboard_document_insert_12', 'dalia_franchise_qna_send_email_when_user_ask_question', 10, 4 );
+
+function dalia_franchise_qna_send_email_when_user_add_comment_to_the_question_article($comment_id, $content_uid, $board) {
+	if (dalia_is_admin()) {
+		return;
+	}
+
+	$content = new KBContent($board->board_id);
+	$content->initWithUID($content_uid);
+	
+	$title = $content->title;
+    $url = new KBUrl();
+
+	$comment = new KBComment();
+	$comment->initWithUID($comment_id);
+	$comment_content = $comment->getContent();
+
+	$emails = ["rpf5573@gmail.com", "rpf5573@kakao.com"];
+
+    // Create the mail instance and set common properties
+    $mail = kboard_mail();
+    $mail->to = $emails;
+    $mail->title = '[신규댓글] ' . $board->board_name . ' - ' . $title;
+    $mail->content = '[댓글내용] ' . "\n" . $comment_content;
+    $mail->url = $url->getDocumentRedirect($content->uid);
+    $mail->url_name = '게시물 보기';
+    $mail->send();
+}
+add_action( 'kboard_comments_insert_12', 'dalia_franchise_qna_send_email_when_user_add_comment_to_the_question_article', 10, 3 );
+
+function dalia_franchise_qna_change_ask_status_to_reask_when_comment_inserted($comment_id, $content_uid, $board) {
+	if (dalia_is_admin()) {
+		return;
+	}
+
+	$content = new KBContent($board->board_id);
+	$content->initWithUID($content_uid);
+
+	$data = [
+		'category2' => '재질문'
+	];
+	$content->updateContent($data);
+}
+add_action( 'kboard_comments_insert_12', 'dalia_franchise_qna_change_ask_status_to_reask_when_comment_inserted', 10, 3 );
