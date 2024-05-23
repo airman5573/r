e67cf80f555eb0dc9df4692f26b83b49e7cf5fa3
Sunny Shoplic <sunny@shoplic.kr>
2024-05-23 14:21:43 +0900

    홈페이지 리뷰에서도 공지사항 클릭시 필수 필드 안보이도록 수정

--- skin/hompage-review/editor.php
+++ skin/hompage-review/editor.php
@@ -35,3 +35,121 @@
 </div>
 
 <?php wp_enqueue_script('kboard-hompage-review-editor', "{$skin_path}/editor.js", array(), KBOARD_VERSION, true)?>
+
+
+<script>
+
+jQuery(($) => {
+  const toggleVisibilityAndRequirement = (elementParent, isHidden, isRequired) => {
+    if (isHidden) {
+      elementParent.addClass('hidden');
+    } else {
+      elementParent.removeClass('hidden');
+    }
+
+    if (isRequired) {
+      elementParent.addClass('required');
+    } else {
+      elementParent.removeClass('required');
+    }
+  };
+
+  const clearInputValue = (inputElement) => {
+    if (inputElement.attr('type') === 'checkbox') {
+      inputElement.prop('checked', false);
+    } else {
+      inputElement.val('');
+    }
+  };
+
+  const clearInputs = () => {
+    const inputsToClear = [
+      'input[name="member_display"]', // Author input
+      'input[name="kboard_option_email"]', // Email input
+      'input[name="kboard_option_agree_checkbox"]', // Agreement input
+      'input[name="secret"]', // Secret checkbox
+    ];
+
+    inputsToClear.forEach((selector) => {
+      const input = $(selector);
+      if (input.length) {
+        clearInputValue(input);
+      }
+    });
+  };
+
+  const setInitialState = (element, isHidden, isRequired) => {
+    clearInputValue(element);
+    const elementParent = element.closest('.kboard-attr-row');
+    if (elementParent.length) {
+        toggleVisibilityAndRequirement(elementParent, isHidden, isRequired);
+    } else {
+      console.error('Parent element not found for', element);
+    }
+  };
+
+  const processNoticeInputChange = (isNotice) => {
+    const elements = [
+      'input[name="member_display"]', // Author input
+      'input[name="kboard_option_email"]', // Email input
+      'input[name="kboard_option_agree_checkbox[]"]', // Agreement input
+      'input[name="password"]' // Password input (assumed for secret posts)
+    ];
+
+    clearInputs();
+
+    elements.forEach(selector => {
+      const element = $(selector);
+      if (element.length) {
+        setInitialState(element, isNotice, !isNotice);
+      } else {
+        console.error('Element not found for', selector);
+      }
+    });
+
+    const passwordInput = $('input[name="password"]');
+    if (passwordInput.length) {
+      toggleVisibilityAndRequirement(passwordInput.closest('.kboard-attr-row'), isNotice, false);
+    }
+  };
+
+  const processSecretCheckboxChange = (isSecret) => {
+    const passwordInput = $('input[name="password"]');
+    if (passwordInput.length) {
+      clearInputValue(passwordInput);
+      toggleVisibilityAndRequirement(passwordInput.closest('.kboard-attr-row'), !isSecret, false);
+    }
+  };
+
+  const noticeInput = $('input[name="notice"]');
+  if (noticeInput.length) {
+    noticeInput.on('change', () => {
+      processNoticeInputChange(noticeInput.prop('checked'));
+    });
+    if (noticeInput.prop('checked')) {
+      processNoticeInputChange(true);
+    }
+  }
+
+  const secretInput = $('input[name="secret"]');
+  if (secretInput.length) {
+    secretInput.on('change', () => {
+      processSecretCheckboxChange(secretInput.prop('checked'));
+    });
+    if (secretInput.prop('checked')) {
+      processSecretCheckboxChange(true);
+    }
+  }
+
+  // Processing code exclusive to admin users
+  <?php
+    // if this user is admin, then uncheck secret checkbox
+    if (dalia_is_admin()) {
+      echo 'secretInput.prop("checked", false);';
+      echo 'processNoticeInputChange(true);';
+      echo 'noticeInput.prop("checked", true);';
+    }
+  ?>
+});
+
+</script>
