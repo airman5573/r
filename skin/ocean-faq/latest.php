bfbc4231c997076b00cde8da7b708d11edd6c8ae
Sunny Shoplic <sunny@shoplic.kr>
2024-05-23 17:02:46 +0900

    backup

--- skin/ocean-faq/latest.php
+++ skin/ocean-faq/latest.php
@@ -1,7 +1,13 @@
 <ul id="kboard-ocean-faq-latest">
 	<?php while($content = $list->hasNext()):?>
 		<li class="kboard-ocean-faq-latest-item cut_strings">
-			<a href="<?php echo $url->getDocumentURLWithUID($content->uid)?>"><?php echo $content->title?></a>
+			<a href="<?php echo $url->getDocumentURLWithUID($content->uid)?>">
+				<div class="kboard-faq-question" onclick="kboard_faq_view_answer('<?php echo $faq_item_index?>')">
+				<!-- 카테고리 불릿 추가 -->
+				<?php dalia_print_category1_name($content); ?>
+				<!-- 카테고리 불릿 추가 -->
+				<?php echo $content->title?><?php if($content->isNew()):?><span class="kboard-hwaikeul-video-slider-new-notify new-mark">N</span><?php endif?></div>
+			</a>
 		</li>
 	<?php endwhile?>
 </ul>
--- skin/qna/latest.php
+++ skin/qna/latest.php
@@ -10,31 +10,35 @@ else{
 	<table>
 		<thead>
 			<tr>
-				<th class="kboard-latest-status"><?php echo __('Status', 'kboard')?></th>
 				<th class="kboard-latest-title"><?php echo __('Title', 'kboard')?></th>
+				<th class="kboard-latest-status"><?php echo __('Status', 'kboard')?></th>
+				<th class="kboard-list-user">작성자</th>
 				<th class="kboard-latest-date"><?php echo __('Date', 'kboard')?></th>
 			</tr>
 		</thead>
 		<tbody>
 			<?php while($content = $list->hasNext()):?>
 			<tr>
-				<td class="kboard-list-status">
+				<td class="kboard-latest-title">
+					<a href="<?php echo $url->set('uid', $content->uid)->set('mod', 'document')->toStringWithPath($board_url)?>" class="flex-item">
+						<?php dalia_print_branch_term_name($content); ?>
+						<div class="kboard-qna-cut-strings">
+
+							<?php if($content->secret):?><i class="xi-lock kboard-icon-lock"></i><?php endif?>
+							<?php echo $content->title?>
+							<span class="kboard-comments-count"><?php echo $content->getCommentsCount()?></span>
+							<?php if($content->isNew()):?><span class="kboard-default-new-notify new-mark">N</span><?php endif?>
+						</div>
+					</a>
+				</td>
+				<td class="kboard-list-status text-center">
 						<?php
 						$answer_status_index = $content->category2 ? array_search($content->category2, $status_list) : '0';
 						$answer_status = $status_list[$answer_status_index];
 						?>
 					<span class="kboard-qna-status status-<?php echo $answer_status_index; ?>"><?php echo $answer_status; ?></span>
 				</td>
-				<td class="kboard-latest-title">
-					<a href="<?php echo $url->set('uid', $content->uid)->set('mod', 'document')->toStringWithPath($board_url)?>">
-						<div class="kboard-qna-cut-strings">
-							<?php if($content->isNew()):?><span class="kboard-qna-new-notify">N</span><?php endif?>
-							<?php if($content->secret):?><img src="<?php echo $skin_path?>/images/icon-lock.png" class="kboard-icon-lock" alt="<?php echo __('Secret', 'kboard')?>"><?php endif?>
-							<?php echo $content->title?>
-							<span class="kboard-comments-count"><?php echo $content->getCommentsCount()?></span>
-						</div>
-					</a>
-				</td>
+				<td class="kboard-list-user text-center"><?php echo apply_filters('kboard_user_display', $content->getUserName(), $content->getUserID(), $content->getUserName(), 'kboard', $boardBuilder)?></td>
 				<td class="kboard-latest-date"><?php echo $content->getDate()?></td>
 			</tr>
 			<?php endwhile?>
