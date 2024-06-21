b844c4d1657c4bfef16e0539e7000b13143746bb
Sunny Shoplic <sunny@shoplic.kr>
2024-06-21 11:55:53 +0900

    관리 프로그램 추가 및 공지사항 대응

--- skin/default-reply-list/editor.php
+++ skin/default-reply-list/editor.php
@@ -1,5 +1,6 @@
 <?php
 $locations = dalia_get_location_with_branches();
+$care_programs = dalia_get_care_programs();
 $fields = $board->fields();
 ?>
 
@@ -90,4 +91,186 @@ jQuery(document).ready(function() {
 		}, 500);
 	}
 });
+</script>
+
+<!-- 프로그램 선택 -->
+<script type="text/javascript">
+jQuery(document).ready(function() {
+	const carePrograms = <?php echo json_encode($care_programs); ?>;
+
+	const primarySelectBox = jQuery("#select-care_program-1");
+	const secondarySelectBox = jQuery("#select-care_program-2");
+	const tertiarySelectBox = jQuery("#select-care_program-3");
+	const tertiaryContainer = jQuery(".select-box-container-3");
+	const hiddenInput = jQuery("#care_program");
+
+	// 기본 옵션 추가
+	function initializeSelectBox(selectBox, options) {
+		selectBox.empty();
+		selectBox.append("<option value=''>프로그램</option>");
+		options.forEach(option => {
+			selectBox.append("<option value='" + option.term_id + "'>" + option.term_name + "</option>");
+		});
+	}
+
+	// 주 select box 초기화
+	initializeSelectBox(primarySelectBox, carePrograms);
+
+	// 주 select box 변경 시
+	primarySelectBox.on("change", function() {
+		const selectedValue = jQuery(this).val();
+		if (selectedValue) {
+			const selectedProgram = carePrograms.find(program => program.term_id == selectedValue);
+			initializeSelectBox(secondarySelectBox, selectedProgram.children);
+			secondarySelectBox.prop("disabled", false);
+			tertiarySelectBox.prop("disabled", true);
+			tertiaryContainer.addClass("d-none");
+			tertiarySelectBox.empty().append("<option value=''>프로그램</option>");
+		} else {
+			secondarySelectBox.prop("disabled", true);
+			secondarySelectBox.empty().append("<option value=''>프로그램</option>");
+			tertiarySelectBox.prop("disabled", true);
+			tertiaryContainer.addClass("d-none");
+			tertiarySelectBox.empty().append("<option value=''>프로그램</option>");
+		}
+	});
+
+	// 중간 select box 변경 시
+	secondarySelectBox.on("change", function() {
+		const primarySelectedValue = primarySelectBox.val();
+		const selectedValue = jQuery(this).val();
+		if (selectedValue) {
+			const primarySelectedProgram = carePrograms.find(program => program.term_id == primarySelectedValue);
+			const selectedProgram = primarySelectedProgram.children.find(program => program.term_id == selectedValue);
+
+			if (selectedProgram.children.length > 0) {
+				initializeSelectBox(tertiarySelectBox, selectedProgram.children);
+				tertiarySelectBox.prop("disabled", false);
+				tertiaryContainer.removeClass("d-none");
+			} else {
+				tertiarySelectBox.prop("disabled", true);
+				tertiaryContainer.addClass("d-none");
+				tertiarySelectBox.empty().append("<option value=''>프로그램</option>");
+				hiddenInput.val(selectedValue); // 자식 항목이 없으면 hidden input에 선택된 값 설정
+			}
+		} else {
+			tertiarySelectBox.prop("disabled", true);
+			tertiaryContainer.addClass("d-none");
+			tertiarySelectBox.empty().append("<option value=''>프로그램</option>");
+		}
+	});
+
+	// 하위 select box 변경 시
+	tertiarySelectBox.on("change", function() {
+		const selectedValue = jQuery(this).val();
+		hiddenInput.val(selectedValue); // 숨겨진 input에 선택된 값 설정
+	});
+});
+
+// 관리자인 경우에 자동으로 필수 필드 삭제 및 공지사항 자동 체크
+jQuery(($) => {
+	const toggleVisibilityAndRequirement = (elementParent, isHidden, isRequired) => {
+		if (isHidden) {
+			$(elementParent).addClass('hidden');
+		} else {
+			$(elementParent).removeClass('hidden');
+		}
+
+		if (isRequired) {
+			$(elementParent).addClass('required');
+		} else {
+			$(elementParent).removeClass('required');
+		}
+	};
+
+	const clearInputValue = (inputElement) => {
+		if ($(inputElement).attr('type') === 'checkbox') {
+			$(inputElement).prop('checked', false);
+		} else {
+			$(inputElement).val('');
+		}
+	};
+
+	const clearInputs = () => {
+		const inputsToClear = [
+			'input[name="kboard_option_email"]',
+			'input[name="kboard_option_agree_checkbox[]"]',
+			'input[name="kboard_option_tel"]',
+			'input[name="kboard_option_branch"]',
+			'select[name="care_program_1"]',
+			'select[name="care_program_2"]',
+			'select[name="care_program_3"]',
+			'select[name="kboard_option_notification_method"]',
+			'select[name="category1"]',
+			'input[name="password"]',
+		];
+
+		inputsToClear.forEach((selector) => {
+				const input = $(selector);
+				input.length > 0 && clearInputValue(input);
+		});
+	};
+
+	const setInitialState = (element, isHidden, isRequired) => {
+		clearInputValue(element);
+		const elementParent = $(element).closest('.kboard-attr-row');
+		toggleVisibilityAndRequirement(elementParent, isHidden, isRequired);
+	};
+
+	const processNoticeInputChange = (isNotice) => {
+		const emailInput = $('input[name="kboard_option_email"]');
+		const agreementInput = $('input[name="kboard_option_agree_checkbox[]"]');
+		const telInput = $('input[name="kboard_option_tel"]');
+		const branchInput = $('input[name="kboard_option_branch"]');
+		const careProgramSelect = $('select[name="care_program_1"]');
+		const notificationMethodSelect = $('select[name="kboard_option_notification_method"]');
+		const category1Select = $('select[name="category1"]');
+		const passwordInput = $('input[name="password"]');
+
+		clearInputs();
+
+		setInitialState(emailInput, isNotice, !isNotice);
+		setInitialState(agreementInput, isNotice, !isNotice);
+		setInitialState(telInput, isNotice, !isNotice);
+		setInitialState(branchInput, isNotice, !isNotice);
+		setInitialState(careProgramSelect, isNotice, !isNotice);
+		setInitialState(notificationMethodSelect, isNotice, !isNotice);
+		setInitialState(category1Select, isNotice, !isNotice);
+
+		toggleVisibilityAndRequirement(passwordInput.closest('.kboard-attr-row'), isNotice, false);
+	};
+
+	const processSecretCheckboxChange = (isSecret) => {
+		const passwordInput = $('input[name="password"]');
+		clearInputValue(passwordInput);
+		toggleVisibilityAndRequirement(passwordInput.closest('.kboard-attr-row'), !isSecret, false);
+	};
+
+	const noticeInput = $('input[name="notice"]');
+		noticeInput.on('change', () => {
+		processNoticeInputChange(noticeInput.is(':checked'));
+	});
+
+	const secretInput = $('input[name="secret"]');
+		secretInput.on('change', () => {
+		processSecretCheckboxChange(secretInput.is(':checked'));
+	});
+
+	if (secretInput.is(':checked')) {
+		processSecretCheckboxChange(true);
+	}
+
+	// Admin-specific processing
+	<?php
+	if (dalia_is_admin()) {
+		echo 'secretInput.prop("checked", false);';
+		echo 'processNoticeInputChange(true);';
+		echo 'noticeInput.prop("checked", true);';
+	}
+	?>
+
+	// Initial state setup
+	// hideFieldsAndCheckNotice();
+});
+
 </script>
