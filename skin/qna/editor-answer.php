952ca7a53e3da9082989416571595b98c738d699
Sunny Shoplic <sunny@shoplic.kr>
2024-06-25 17:57:51 +0900

    backup

--- skin/qna/editor-answer.php
+++ skin/qna/editor-answer.php
@@ -141,15 +141,15 @@ $original_question_content = $original_question_content->initWithUID($parent_uid
 		<div class="kboard-control">
 			<div class="left">
 				<?php if($content->uid):?>
-				<a href="<?php echo esc_url($url->getDocumentURLWithUID($content->uid))?>" class="kboard-default-button-small"><?php echo __('Back', 'kboard')?></a>
-				<a href="<?php echo esc_url($url->getBoardList())?>" class="kboard-default-button-small"><?php echo __('List', 'kboard')?></a>
+				<a href="<?php echo $url->getDocumentURLWithUID($content->uid)?>" class="kboard-qna-button-small dalia-btn-01"><?php echo __('Back', 'kboard')?></a>
+				<a href="<?php echo $url->getBoardList()?>" class="kboard-qna-button-small dalia-btn-01"><?php echo __('List', 'kboard')?></a>
 				<?php else:?>
-				<a href="<?php echo esc_url($url->getBoardList())?>" class="kboard-default-button-small"><?php echo __('Back', 'kboard')?></a>
+				<a href="<?php echo $url->getBoardList()?>" class="kboard-qna-button-small dalia-btn-01"><?php echo __('Back', 'kboard')?></a>  
 				<?php endif?>
 			</div>
 			<div class="right">
 				<?php if($board->isWriter()):?>
-				<button type="submit" class="kboard-default-button-small"><?php echo __('Save', 'kboard')?></button>
+				<button type="submit" class="kboard-qna-button-small dalia-btn-01"><?php echo __('Save', 'kboard')?></button>
 				<?php endif?>
 			</div>
 		</div>
